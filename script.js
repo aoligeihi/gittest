3ec7b6f49c74a500ada2164e265e092e91c74627
张宇阳 <206311428@qq.com>
2023-12-29 09:24:55 +0800

    555

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ const content = fs.readFileSync('./README.md', {
     encoding: 'utf-8'
 });
 
-console.log(content);
+// console.log(content);
+
+
+const newContent = content.replace('此处是不想提交的', '');
+
+fs.writeFileSync('./index.md', newContent);
